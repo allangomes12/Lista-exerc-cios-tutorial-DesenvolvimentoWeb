cb34b812acd7d2f555290be5005abe881ee16705
allan <allanpoul12@gmail.com>
2026-05-30 19:28:59 -0300

    Questão 2

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Bem-vindo ao sistema")
 });
 
+app.get("/sobre", (req,res) => {
+    res.send("Esta é a porta SOBRE")
+});
+
 app.listen(8081, () => {
     console.log("Servidor rodando")
-})
+});
