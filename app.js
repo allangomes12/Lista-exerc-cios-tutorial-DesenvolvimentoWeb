d1d5340660fdfebad3d8e7324e59352ae9f4750f
allan <allanpoul12@gmail.com>
2026-05-30 19:51:51 -0300

    Questão 4

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const app = express()
 
+// questão 1
 app.get("/", (req, res) => {
     res.send("Bem-vindo ao sistema")
 });
 
+// questão 2
 app.get("/sobre", (req,res) => {
     res.send("Esta é a porta SOBRE")
 });
 
+// questão 3
 app.get("/contato", (req, res) => {
     res.json({
         email: "contato@gmail.com",
         telefone: "(81) 99999-9999"
     });
+});
+
+// questão 4 
+app.get("/erro", (req, res) => {
+    res.status(404).send("Página não encontrada")
 });
 
 app.listen(8081, () => {
